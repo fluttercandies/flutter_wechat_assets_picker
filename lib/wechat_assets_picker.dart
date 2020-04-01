1b277a832961ccb2f3de60bb9046533fc756f233
AlexVincent525 <github@alexv525.com>
2020-04-01 14:52:24 +0800

    :art: Export `AssetEntity` from `photo_manager`.

--- lib/wechat_assets_picker.dart
+++ lib/wechat_assets_picker.dart
@@ -1,5 +1,7 @@
 library wechat_assets_picker;
 
+export 'package:photo_manager/photo_manager.dart' show AssetEntity;
+
 export 'src/provider/asset_entity_image_provider.dart';
 export 'src/provider/asset_picker_provider.dart';
 export 'src/provider/asset_picker_viewer_provider.dart';
