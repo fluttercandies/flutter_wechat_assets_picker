bdae33a8109fe088be83984716145044a77c7235
AlexVincent525 <github@alexv525.com>
2020-04-07 11:52:38 +0800

    :zap: Export `RequestType`.

--- lib/wechat_assets_picker.dart
+++ lib/wechat_assets_picker.dart
@@ -1,6 +1,6 @@
 library wechat_assets_picker;
 
-export 'package:photo_manager/photo_manager.dart' show AssetEntity;
+export 'package:photo_manager/photo_manager.dart' show AssetEntity, RequestType;
 
 export 'src/provider/asset_entity_image_provider.dart';
 export 'src/provider/asset_picker_provider.dart';
