46ba48de1174fed527c27ab1a07619b6038fbc83
AlexVincent525 <github@alexv525.com>
2020-04-07 14:55:37 +0800

    :alien: Export `TextDelegate`.

--- lib/wechat_assets_picker.dart
+++ lib/wechat_assets_picker.dart
@@ -2,6 +2,7 @@ library wechat_assets_picker;
 
 export 'package:photo_manager/photo_manager.dart' show AssetEntity, RequestType;
 
+export 'src/constants/text_delegate.dart';
 export 'src/provider/asset_entity_image_provider.dart';
 export 'src/provider/asset_picker_provider.dart';
 export 'src/provider/asset_picker_viewer_provider.dart';
