8526e3d8b707e28c1c37f991d89786920bcf6645
AlexVincent525 <github@alexv525.com>
2020-04-15 13:22:40 +0800

    :recycle: Refactoring page transition builder.

--- lib/src/constants/slide_page_transition_builder.dart
+++ lib/src/constants/slide_page_transition_builder.dart
@@ -6,43 +6,54 @@ import 'package:flutter/material.dart';
 
 /// Built a slide page transition for the picker.
 /// 为选择器构造一个上下进出的页面过渡动画
-class SlidePageTransitionBuilder<T> extends PageRouteBuilder<T> {
-  SlidePageTransitionBuilder({@required this.builder})
-      : super(
-          pageBuilder: (
-            BuildContext _,
-            Animation<double> __,
-            Animation<double> ___,
-          ) =>
-              builder,
-        );
+class SlidePageTransitionBuilder<T> extends PageRoute<T> {
+  SlidePageTransitionBuilder({
+    @required this.builder,
+    this.transitionCurve = Curves.easeIn,
+    this.transitionDuration = const Duration(milliseconds: 500),
+  });
 
   final Widget builder;
 
-  @override
-  Duration get transitionDuration => const Duration(milliseconds: 500);
+  final Curve transitionCurve;
 
   @override
-  RoutePageBuilder get pageBuilder => (
-        BuildContext _,
-        Animation<double> __,
-        Animation<double> ___,
-      ) =>
-          builder;
+  final Duration transitionDuration;
 
   @override
-  RouteTransitionsBuilder get transitionsBuilder => (
-        BuildContext _,
-        Animation<double> animation,
-        Animation<double> __,
-        Widget child,
-      ) {
-        return SlideTransition(
-          position: Tween<Offset>(
-            begin: const Offset(0, 1),
-            end: Offset.zero,
-          ).animate(CurvedAnimation(curve: Curves.easeIn, parent: animation)),
-          child: child,
-        );
-      };
+  final bool opaque = true;
+
+  @override
+  final bool barrierDismissible = false;
+
+  @override
+  final bool maintainState = true;
+
+  @override
+  Color get barrierColor => null;
+
+  @override
+  String get barrierLabel => null;
+
+  @override
+  Widget buildPage(BuildContext context, Animation<double> animation,
+      Animation<double> secondaryAnimation) {
+    return builder;
+  }
+
+  @override
+  Widget buildTransitions(
+    BuildContext context,
+    Animation<double> animation,
+    Animation<double> secondaryAnimation,
+    Widget child,
+  ) {
+    return SlideTransition(
+      position: Tween<Offset>(
+        begin: const Offset(0, 1),
+        end: Offset.zero,
+      ).animate(CurvedAnimation(curve: transitionCurve, parent: animation)),
+      child: child,
+    );
+  }
 }
