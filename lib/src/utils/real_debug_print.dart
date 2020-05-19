9acd3284b82d449ab756bdc3d7b6f0007b486dab
AlexVincent525 <github@alexv525.com>
2020-05-19 14:51:10 +0800

    :recycle: Using log to log.

--- lib/src/utils/real_debug_print.dart
+++ lib/src/utils/real_debug_print.dart
@@ -2,12 +2,14 @@
 /// [Author] Alex (https://github.com/AlexVincent525)
 /// [Date] 2020/4/20 10:36
 ///
+import 'dart:developer';
+
 import 'package:flutter/foundation.dart';
 
 /// Print message only if at [kDebugMode].
 /// 仅在debug模式下输出消息
-void realDebugPrint(String message, {int wrapWidth}) {
+void realDebugPrint(dynamic message) {
   if (kDebugMode) {
-    debugPrint(message, wrapWidth: wrapWidth);
+    log('$message');
   }
 }
