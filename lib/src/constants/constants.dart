def44c51333537c26ae78a12a5d2b1356f1c85d1
Alex <github@alexv525.com>
2020-05-30 15:12:51 +0800

    :truck: Make delegates package.

--- lib/src/constants/constants.dart
+++ lib/src/constants/constants.dart
@@ -2,7 +2,7 @@
 /// [Author] Alex (https://github.com/AlexVincent525)
 /// [Date] 2020/3/31 16:02
 ///
-import 'text_delegate.dart';
+import '../delegates/text_delegate.dart';
 
 export '../delegates/sort_path_delegate.dart';
 export '../delegates/text_delegate.dart';
@@ -11,7 +11,6 @@ export '../utils/real_debug_print.dart';
 export 'colors.dart';
 export 'custom_scroll_physics.dart';
 export 'screens.dart';
-export 'text_delegate.dart';
 
 class Constants {
   const Constants._();
--- lib/wechat_assets_picker.dart
+++ lib/wechat_assets_picker.dart
@@ -2,7 +2,8 @@ library wechat_assets_picker;
 
 export 'package:photo_manager/photo_manager.dart' show AssetEntity, RequestType;
 
-export 'src/constants/text_delegate.dart';
+export 'src/delegates/sort_path_delegate.dart';
+export 'src/delegates/text_delegate.dart';
 export 'src/provider/asset_entity_image_provider.dart';
 export 'src/provider/asset_picker_provider.dart';
 export 'src/provider/asset_picker_viewer_provider.dart';
