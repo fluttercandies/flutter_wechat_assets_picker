4b7dfb99fb333abee4947daa2bf4f2a7da7ee29d
Yaniv Shaked <shakedyaniv@gmail.com>
2021-05-02 12:52:18 +0300

    [Fix] When preview an image, always use the original, not the thumb

--- lib/src/delegates/asset_picker_builder_delegate.dart
+++ lib/src/delegates/asset_picker_builder_delegate.dart
@@ -1283,7 +1283,6 @@ class DefaultAssetPickerBuilderDelegate
             currentIndex: _index,
             previewAssets: _current,
             themeData: theme,
-            previewThumbSize: previewThumbSize,
             selectedAssets: _selected,
             selectorProvider: provider as DefaultAssetPickerProvider,
             specialPickerType: specialPickerType,
