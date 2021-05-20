29968590184978a9df005e3350abebc06bd56fbf
Alex Li <github@alexv525.com>
2021-05-20 14:44:28 +0800

    🎨 Format extensions

--- lib/src/constants/extensions.dart
+++ lib/src/constants/extensions.dart
@@ -6,11 +6,13 @@ import 'package:flutter/material.dart';
 
 extension BuildContextExtension on BuildContext {
   MediaQueryData get mediaQuery => MediaQuery.of(this);
+
   ThemeData get themeData => Theme.of(this);
 }
 
 extension BrightnessExtension on Brightness {
   bool get isDark => this == Brightness.dark;
+
   bool get isLight => this == Brightness.light;
 }
 
