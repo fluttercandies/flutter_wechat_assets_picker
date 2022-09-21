d8e9ea53b314cdeee31521bc7c64d8b62b745ecf
Alex Li <github@alexv525.com>
2022-09-21 10:12:43 +0800

    🐛 Fix conditions with the confirm button (#367)

--- test/test_utils.dart
+++ test/test_utils.dart
@@ -1,52 +1,62 @@
-// Copyright 2019 The FlutterCandies author. All rights reserved.
-// Use of this source code is governed by an Apache license that can be found
-// in the LICENSE file.
+//
+// [Author] Alex (https://github.com/AlexV525)
+// [Date] 2022/09/20 16:35
+//
 
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:wechat_assets_picker/wechat_assets_picker.dart';
 
-void main() {
-  PhotoManager.withPlugin(TestPhotoManagerPlugin());
-  AssetPicker.setPickerDelegate(TestAssetPickerDelegate());
+const String _testButtonText = 'Picker test press';
 
-  final Finder defaultButtonFinder = find.byType(TextButton);
+final Finder defaultButtonFinder = find.widgetWithText(
+  TextButton,
+  _testButtonText,
+);
 
-  Widget _defaultApp({void Function(BuildContext)? onButtonPressed}) {
-    return MaterialApp(
-      home: Builder(
-        builder: (BuildContext context) => Scaffold(
-          body: Center(
-            child: TextButton(
-              onPressed: () => onButtonPressed?.call(context),
-              child: const Text('Press'),
-            ),
-          ),
+Widget defaultPickerTestApp({
+  void Function(BuildContext)? onButtonPressed,
+  Locale locale = const Locale('zh'),
+}) {
+  return MaterialApp(
+    home: _DefaultHomePage(onButtonPressed),
+    localizationsDelegates: const <LocalizationsDelegate<dynamic>>[
+      GlobalWidgetsLocalizations.delegate,
+      GlobalMaterialLocalizations.delegate,
+      GlobalCupertinoLocalizations.delegate,
+    ],
+    supportedLocales: const <Locale>[
+      Locale('zh'),
+      Locale('en'),
+      Locale('he'),
+      Locale('de'),
+      Locale('ru'),
+      Locale('ja'),
+      Locale('ar'),
+      Locale('fr'),
+    ],
+    locale: locale,
+  );
+}
+
+class _DefaultHomePage extends StatelessWidget {
+  const _DefaultHomePage(this.onButtonPressed, {Key? key}) : super(key: key);
+
+  final void Function(BuildContext)? onButtonPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: TextButton(
+          onPressed: () => onButtonPressed?.call(context),
+          child: const Text(_testButtonText),
         ),
       ),
     );
   }
-
-  testWidgets('PathNameBuilder called correctly', (WidgetTester tester) async {
-    await tester.pumpWidget(
-      _defaultApp(
-        onButtonPressed: (BuildContext context) {
-          AssetPicker.pickAssets(
-            context,
-            pickerConfig: AssetPickerConfig(
-              pathNameBuilder: (AssetPathEntity p) => 'testPathNameBuilder',
-            ),
-          );
-        },
-      ),
-    );
-    await tester.tap(defaultButtonFinder);
-    await tester.pumpAndSettle();
-    await tester.tap(find.byIcon(Icons.keyboard_arrow_down));
-    await tester.pumpAndSettle();
-    expect(find.text('testPathNameBuilder'), findsOneWidget);
-  });
 }
 
 class TestPhotoManagerPlugin extends PhotoManagerPlugin {
