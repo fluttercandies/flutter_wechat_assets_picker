683345b380cc562a139383252af8cfad3264c125
Alex Li <github@alexv525.com>
2022-10-04 00:42:41 +0800

    🐛 Fix confirm button when no preview

--- lib/src/delegates/asset_picker_builder_delegate.dart
+++ lib/src/delegates/asset_picker_builder_delegate.dart
@@ -900,7 +900,8 @@ class DefaultAssetPickerBuilderDelegate
       //   because any click on an asset selects it.
       // - On iOS and macOS, show nothing.
       actions: <Widget>[
-        if (!isAppleOS && isPreviewEnabled) confirmButton(context),
+        if (!isAppleOS && (isPreviewEnabled || !isSingleAssetMode))
+          confirmButton(context),
       ],
       actionsPadding: const EdgeInsetsDirectional.only(end: 14),
       blurRadius: isAppleOS ? appleOSBlurRadius : 0,
