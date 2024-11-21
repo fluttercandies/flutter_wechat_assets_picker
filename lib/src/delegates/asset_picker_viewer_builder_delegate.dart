dc5911e89705854cc7ffbacf0c8be372fca4adfc
易大宝 <yishangfei.me@gmail.com>
2024-11-21 23:10:23 +0800

    Fix selecting when reached the max asset limit in the preview (#648)
    Fixes #607
    
    ---------
    
    Co-authored-by: Alex Li <github@alexv525.com>

--- lib/src/delegates/asset_picker_viewer_builder_delegate.dart
+++ lib/src/delegates/asset_picker_viewer_builder_delegate.dart
@@ -272,7 +272,7 @@ abstract class AssetPickerViewerBuilderDelegate<Asset, Path> {
   }
 
   void selectAsset(Asset entity) {
-    if (maxAssets != null && selectedCount >= maxAssets!) {
+    if (maxAssets != null && selectedCount > maxAssets!) {
       return;
     }
     provider?.selectAsset(entity);
