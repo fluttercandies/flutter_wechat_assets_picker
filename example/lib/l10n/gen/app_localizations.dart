9ad3d369aae76621cfe0dfca3a5f2d58bc842023
Gasol Wu <gasol.wu@gmail.com>
2024-12-10 20:53:17 +0800

    feat: Add Traditional Chinese (zh_Hant) in example app

--- example/lib/l10n/gen/app_localizations.dart
+++ example/lib/l10n/gen/app_localizations.dart
@@ -95,6 +95,7 @@ abstract class AppLocalizations {
   /// A list of this localizations delegate's supported locales.
   static const List<Locale> supportedLocales = <Locale>[
     Locale('en'),
+    Locale.fromSubtags(languageCode: 'zh', scriptCode: 'Hant'),
     Locale('zh')
   ];
 
@@ -447,6 +448,18 @@ class _AppLocalizationsDelegate
 }
 
 AppLocalizations lookupAppLocalizations(Locale locale) {
+  // Lookup logic when language+script codes are specified.
+  switch (locale.languageCode) {
+    case 'zh':
+      {
+        switch (locale.scriptCode) {
+          case 'Hant':
+            return AppLocalizationsZhHant();
+        }
+        break;
+      }
+  }
+
   // Lookup logic when only language code is specified.
   switch (locale.languageCode) {
     case 'en':
