7835de8788eeeac3efec66668791700576a68789
Alex Li <github@alexv525.com>
2024-12-11 14:19:44 +0800

    🚀 Support matching script and country code of locales with the text delegate (#668)
    Prepare for #667

--- test/delegates_test.dart
+++ test/delegates_test.dart
@@ -2,10 +2,40 @@
 // Use of this source code is governed by an Apache license that can be found
 // in the LICENSE file.
 
+import 'package:flutter/rendering.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:wechat_assets_picker/wechat_assets_picker.dart';
 
 void main() {
+  group(AssetPickerTextDelegate, () {
+    test('returns the default when available', () {
+      expect(
+        assetPickerTextDelegateFromLocale(null),
+        equals(const AssetPickerTextDelegate()),
+      );
+      expect(
+        assetPickerTextDelegateFromLocale(const Locale('zh')),
+        equals(const AssetPickerTextDelegate()),
+      );
+      expect(
+        assetPickerTextDelegateFromLocale(const Locale('zxx')),
+        equals(const AssetPickerTextDelegate()),
+      );
+    });
+
+    test('each delegate can be obtained by its locale definition', () {
+      for (final delegate in assetPickerTextDelegates) {
+        final locale = Locale.fromSubtags(
+          languageCode: delegate.languageCode,
+          scriptCode: delegate.scriptCode,
+          countryCode: delegate.countryCode,
+        );
+        final matchedDelegate = assetPickerTextDelegateFromLocale(locale);
+        expect(matchedDelegate, equals(delegate));
+      }
+    });
+  });
+
   test('Sort paths correctly', () {
     final List<PathWrapper<AssetPathEntity>> paths =
         <PathWrapper<AssetPathEntity>>[
