dfa8352e6e04a622cc47f40f82987d2d1c610bcf
WeiJun0507 <junwei190528@gmail.com>
2024-12-23 23:23:16 +0800

    🐛 Fix drag select index accuracy when asset size is one page

--- lib/src/delegates/asset_grid_drag_selection_coordinator.dart
+++ lib/src/delegates/asset_grid_drag_selection_coordinator.dart
@@ -130,6 +130,15 @@ class AssetGridDragSelectionCoordinator {
       }
     }
 
+    final double dividedSpacing = delegate.itemSpacing / gridCount;
+    final double anchor = math.min(
+      (maxRow * (itemSize + dividedSpacing) +
+              topPadding -
+              delegate.itemSpacing) /
+          dimensionSize.height,
+      1,
+    );
+
     int rowIndex = _getDragPositionIndex(
       switch (gridRevert) {
         true => dimensionSize.height -
@@ -143,19 +152,18 @@ class AssetGridDragSelectionCoordinator {
       itemSize,
     );
 
-    if (placeholderCount > 0) {
+    final double initialFirstPosition = dimensionSize.height * anchor;
+    if (gridRevert && dimensionSize.height > initialFirstPosition) {
+      final deductedRow =
+          (dimensionSize.height - initialFirstPosition) ~/ itemSize;
+      rowIndex -= deductedRow;
+    }
+
+    if (placeholderCount > 0 && maxRow > maxRowPerPage) {
       rowIndex -= 1;
     }
 
-    // Correct the row index when asset length is less than one page
-    if (maxRowPerPage > maxRow) {
-      rowIndex = maxRow - (maxRowPerPage - rowIndex);
-    }
-
     final currentDragIndex = rowIndex * gridCount + columnIndex;
-
-    print(
-        "Check drag index: $currentDragIndex | $initialSelectingIndex | $rowIndex | $columnIndex");
 
     // Check the selecting index in order to diff unselecting assets.
     smallestSelectingIndex = math.min(
@@ -164,7 +172,7 @@ class AssetGridDragSelectionCoordinator {
     );
     smallestSelectingIndex = math.max(0, smallestSelectingIndex);
     largestSelectingIndex = math.max(
-      currentDragIndex + 1,
+      currentDragIndex,
       largestSelectingIndex,
     );
 
@@ -191,7 +199,7 @@ class AssetGridDragSelectionCoordinator {
       filteredAssetList = provider.currentAssets.getRange(
         math.max(0, initialSelectingIndex),
         math.min(
-          currentDragIndex + 1,
+          currentDragIndex + (maxRow > maxRowPerPage ? 1 : 0),
           provider.currentAssets.length,
         ),
       );
